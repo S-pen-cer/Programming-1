9cda814f882eeed5cd98554a9356d7540151eb2f
S-pen-cer <20260814@mywhitecliffe.com>
2026-06-10 11:41:10 +1200

    Add files via upload
    These are initial drafts. Still trying to understand relationships between objects and methods, while trying to make progress with Assessment.
    Coded on different PC. Other renditions made in other PC's might make this moot.

---
+++ test2.py
@@ -0,0 +1,21 @@
+from test1 import Registry
+'''
+Staff_Info = []
+def add_requisition (self):
+    date = input("Please enter date")
+    name = input("Please enter name")
+    staff_id = input("Please enter staff ID")
+    staff_info = [date, name, staff_id]
+    Staff_Info.append(staff_info)
+    print (Staff_Info)
+
+add_requisition(Staff_Info)
+'''
+
+def shopping_list(self):
+    for si in Registry.staff_info:
+        input("Please enter item")
+        float(input("Please enter price"))
+        int(input("Please enter quantity"))
+
+
