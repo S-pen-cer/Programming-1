6ed20b7e85d7f3b481ad4a3128e1e7b6803de4f4
S-pen-cer <20260814@mywhitecliffe.com>
2026-06-16 10:40:28 +1200

    Add files via upload
    Somewhat different approach: methods in same file as Class. Managed to make inputs work.

--- test3.py
+++ test3.py
@@ -3,7 +3,7 @@
 class Registry:
     request_count = 0
     staff_info = []
-    def __init__(self, Date, Name, Staff_ID, Order_Status):
+    def __init__(self):
         Date = input("Please input the date")
         Name = input("Please input a name")
         Staff_ID = input("Please input a Staff ID")
@@ -23,8 +23,11 @@ class Registry:
             print(f"Items:{item} amounts to {current_cost}")
 
 registry1 = Registry()
-#Still wrong, might try another way
-#If coding another method with inputs for parameters (Date, Name, Staff ID, Status), doesn't that
-#invalidate the need for initialization? Or does initialized parameters act as placeholders?
+#Seems to work with inputs now; Might be better to put methods in one file instead of multiple
+#Note: Class in one file and methods in another is not the same as calling as Class in another
+#file. The latter is said to just make a function that uses the Class, and is not a method itself
+
+
+
 
 
